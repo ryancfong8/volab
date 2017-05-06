c984aa081bad58055d874a89ede18403f2a7744b
Tyler Weng <tylercweng@gmail.com>
2017-05-06 16:49:21 -0700

    Add basic auth

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: decks
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Deck < ApplicationRecord
   has_many :flashcards
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,11 @@
 # == Schema Information
 #
-# Table name: decks
+# Table name: users
 #
 #  id         :integer          not null, primary key
-#  title      :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 
-require 'test_helper'
-
-class DeckTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+class User < ApplicationRecord
 end
